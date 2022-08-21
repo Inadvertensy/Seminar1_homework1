3bdb620c985ed34b6a0a992d0343f03ee39cfeeb
1 1 <bobra2@ya.ru>
2022-08-21 10:57:16 +0300

    task 3 added

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число 
+// и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Please enter number");
+int number1=Convert.ToInt32(Console.ReadLine());
+int divRem=number1%2;
+//Console.WriteLine(divRem);
+if (divRem==0)
+{
+    Console.Write("Number ");
+    Console.Write(number1);
+    Console.Write(" is even");
+
+}
+else 
+{
+    Console.Write("Number ");
+    Console.Write(number1);
+    Console.Write(" is not even");
+}
